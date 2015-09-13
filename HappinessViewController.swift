30bbdc10afe53315a8a24d3f0621f064f17aea01
nevercry <ysfnevercry@gmail.com>
2015-09-13 17:37:11 +0800

    Happyness Demo
    init commit

---
+++ HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by nevercry on 2/8/15.
+//  Copyright (c) 2015 nevercry. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+   
+}
